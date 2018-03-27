062ae599c6fddb797eed4ad632a72ea6f53afdbe
DartPower <SIMPSON-NEKEK@mail.ru>
2018-03-27 11:47:08 +0300

    Oh my...

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 SetLocal EnableExtensions EnableDelayedExpansion
 color 09
-title Compressing. Please wait... [Approximately 2-5 mins]
+title Compressing. Please wait... [Approximately 10 secs]
 cd pk3_data
 ..\7za a -t7z -m0=lzma2 -mx=9 -aoa "..\Friendly_Team-%date:~6,4%%date:~3,2%%date:~0,2%-%time:~0,2%%time:~3,2%.pk7" *
 cls
